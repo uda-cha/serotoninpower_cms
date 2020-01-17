ec4f21b543e1385dd9887d0467fc72f8a3902d90
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-01-18 04:37:18 +0900

    restrict CRUD operations to index, new, create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  #before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
@@ -9,8 +9,8 @@ class CommentsController < ApplicationController
 
   # GET /comments/1
   # GET /comments/1.json
-  def show
-  end
+  #def show
+  #end
 
   # GET /comments/new
   def new
@@ -18,8 +18,8 @@ class CommentsController < ApplicationController
   end
 
   # GET /comments/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /comments
   # POST /comments.json
@@ -39,33 +39,33 @@ class CommentsController < ApplicationController
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
-  def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @comment.update(comment_params)
+  #      format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+  #      format.json { render :show, status: :ok, location: @comment }
+  #    else
+  #      format.html { render :edit }
+  #      format.json { render json: @comment.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
-  def destroy
-    @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+  #  @comment.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+    #def set_comment
+    #  @comment = Comment.find(params[:id])
+    #end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
