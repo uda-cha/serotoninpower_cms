d9f39c106eeeb0484e7e2ffd9e93249783b78e8c
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-01-19 14:38:50 +0900

    set post_id and public when comment has been created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,14 +25,16 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+    @comment.public = true
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        format.html { redirect_to comments_path, notice: 'Comment was successfully created.' }
+        #format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        #format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -69,6 +71,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:post_id, :name, :public, :content)
+      params.require(:comment).permit(:name, :content)
     end
 end
