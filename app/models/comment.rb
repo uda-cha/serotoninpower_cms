587370cdb500358516ccc7e89f5b95cfc95b3e9d
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-02-03 21:25:27 +0900

    change validation method for boolean

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@ require 'resolv'
 class Comment < ApplicationRecord
   validates :post_id,  presence: true, length: { maximum: 255 }
   validates :name,     presence: true, length: { maximum: 20 }
-  validates :public,   presence: true
+  validates :public,   inclusion: { in: [true, false] }
   validates :content,  presence: true, length: { maximum: 1200 }
   validates :ipadress, presence: true, format: { with: Resolv::IPv4::Regex }
-  validates :admin,    presence: true
+  validates :admin,    inclusion: { in: [true, false] }
 end
