16913a1aac404215fece744850fe9d376ee0701e
uda-cha <mine_s@zd6.so-net.ne.jp>
2020-02-13 00:27:48 +0900

    fix spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -15,11 +15,18 @@ RSpec.describe CommentsController, type: :controller do
     end
   end
 
+  describe '#wrapper' do
+    it "ステータスコード200が返ること" do
+      get :wrapper, params: { post_id: 'test' }
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe '#create' do
-    it "ステータスコード302が返ること & newへリダイレクトされること" do
+    it "ステータスコード302が返ること & wrapperへリダイレクトされること" do
       post :create, params: { post_id: 'test', comment: { name: "tester", content: "ThisIsTest" } }
       expect(response).to have_http_status(302)
-      expect(response).to redirect_to(new_comment_path)
+      expect(response).to redirect_to(comments_wrapper_path)
     end
   end
 end
